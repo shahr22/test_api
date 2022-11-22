2cfb5150d1b7fea8ea1e893fc0390752763d7d05
Shah Rahman <srahman@verticalrelevance.com>
2022-11-22 15:46:19 -0500

    first commit

--- vr_api/vr_api_stack.py
+++ vr_api/vr_api_stack.py
@@ -1,19 +1,40 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_apigateway as apigw,
+    aws_logs as logs,
 )
+
 from constructs import Construct
 
 class VrApiStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        log_group = logs.LogGroup(self, "ApiGatewayAccessLogs")
 
-        # The code that defines your stack goes here
+        api = apigw.RestApi(self, "PowerAutomateAPI",
+            deploy_options=apigw.StageOptions(
+                access_log_destination=apigw.LogGroupLogDestination(log_group),
+                access_log_format=apigw.AccessLogFormat.clf(),
+            ),
+            endpoint_configuration=apigw.EndpointConfiguration(
+                types=[apigw.EndpointType.REGIONAL]
+            )
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "VrApiQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        api_key = api.add_api_key(
+            id="ApiKey",
+            api_key_name="pa-key"
+        )
+
+        plan = api.add_usage_plan("UsagePlan",
+            name="Easy",
+            throttle=apigw.ThrottleSettings(
+                rate_limit=10,
+                burst_limit=2
+            )
+        )
+
+        plan.add_api_key(api_key)
+
+        plan.add_api_stage(stage=api.deployment_stage)
